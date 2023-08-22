2f4001732e4a6db7182b9e7742c486aa311ddc33
DNMetodiev <91205942+DNMetodiev@users.noreply.github.com>
2023-08-22 23:48:34 +0300

    Add Firebase config

--- addonis-prj/src/App.jsx
+++ addonis-prj/src/App.jsx
@@ -1,12 +1,25 @@
-import React from "react";
 import HomeContent from "./components/views/HomeContent/HomeContent";
+import { useState } from "react";
+import { BrowserRouter } from 'react-router-dom';
+import { AppContext } from '../src/context/AppContext';
+
 
 function App() {
+  const [appState, setAppState] = useState({
+    use: null,
+    userData: null,
+  });
+
   return (
-    <div>
-      <HomeContent />
-    </div>
+    <AppContext.Provider value={{ ...appState, setContext: setAppState }}>
+      <BrowserRouter>
+        <div className="App">
+          <HomeContent />
+        </div>
+      </BrowserRouter>
+    </AppContext.Provider>
   );
+
 }
 
 export default App;
---
+++ addonis-prj/src/config/firebase-config.js
@@ -0,0 +1,22 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getDatabase } from 'firebase/database';
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDtkqYHqPVNTbfyaBrshArGni10LiapKnw",
+  authDomain: "addonis-e5b1b.firebaseapp.com",
+  projectId: "addonis-e5b1b",
+  storageBucket: "addonis-e5b1b.appspot.com",
+  messagingSenderId: "445136249009",
+  appId: "1:445136249009:web:00a98b528364013fa64290",
+
+  databaseURL: "https://addonis-e5b1b-default-rtdb.europe-west1.firebasedatabase.app/"
+};
+
+
+export const app = initializeApp(firebaseConfig);
+// the Firebase authentication handler
+export const auth = getAuth(app);
+// the Realtime Database handler
+export const db = getDatabase(app);
