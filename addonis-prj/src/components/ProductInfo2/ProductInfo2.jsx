f99033a214b4bf8670e0db539feb181b8adc8637
DNMetodiev <91205942+DNMetodiev@users.noreply.github.com>
2023-09-12 21:29:04 +0300

    Add Subscribe for app release option

--- addonis-prj/src/components/ProductInfo2/ProductInfo2.jsx
+++ addonis-prj/src/components/ProductInfo2/ProductInfo2.jsx
@@ -8,13 +8,14 @@ import {
   Stack,
   Icon,
   Button,
-  StackDivider,
+  Input,
   useColorModeValue,
   Popover,
   PopoverTrigger,
   PopoverContent,
   PopoverBody,
   PopoverArrow,
+  StackDivider,
 } from "@chakra-ui/react";
 import {
   IoAlertCircleOutline,
@@ -23,9 +24,11 @@ import {
   IoPhonePortraitOutline,
   IoCloudDownloadOutline,
 } from "react-icons/io5";
-import { MdFileDownload, } from "react-icons/md"
+import { MdFileDownload } from "react-icons/md";
 import animationData from "../../assets/animation_lls8yso6.json";
 import Lottie from "lottie-react";
+import { ref, push } from 'firebase/database';
+import { db } from '../../config/firebase-config';
 
 const popoverData = [
   {
@@ -57,13 +60,20 @@ const popoverData = [
 
 export default function ProductInfo2() {
   const [activePopover, setActivePopover] = useState(null);
+  const [email, setEmail] = useState("");
+  const [isSubscribed, setIsSubscribed] = useState(false);
 
-  const handleMouseEnter = (index) => {
-    setActivePopover(index);
-  };
+  const handleMouseEnter = (index) => setActivePopover(index);
+  const handleMouseLeave = () => setActivePopover(null);
 
-  const handleMouseLeave = () => {
-    setActivePopover(null);
+  const handleSubscription = async () => {
+    try {
+      const subscriptionRef = ref(db, 'subscriptions');
+      await push(subscriptionRef, { email });
+      setIsSubscribed(true);
+    } catch (error) {
+      console.error("Error subscribing: ", error);
+    }
   };
 
   return (
@@ -76,15 +86,9 @@ export default function ProductInfo2() {
           <Heading color={"gray"} textAlign={"center"}>
             Stay Informed and In Control with Our App!
           </Heading>
-
           <Text color={"black"} fontSize={"lg"} textAlign={"center"}>
-            Introducing the ultimate solution for managing your smart home
-            products. Our app brings the power of control right to your
-            fingertips, ensuring you never miss a beat. With instant
-            notifications and real-time updates, you're always in the loop about
-            what's happening in your home.
+            Introducing the ultimate solution...
           </Text>
-
           {popoverData.map((popover, index) => (
             <Popover
               key={index}
@@ -110,19 +114,33 @@ export default function ProductInfo2() {
               </PopoverContent>
             </Popover>
           ))}
-
           <Stack
             spacing={4}
-            divider={
-              <StackDivider
-                borderColor={useColorModeValue("gray.100", "gray.700")}
-              />
-            }
+            divider={<StackDivider borderColor={useColorModeValue("gray.100", "gray.700")} />}
           >
-            <Text color={"black"} fontSize={"lg"} textAlign={"center"}>Experience the future of smart living with our app. Download now and unlock a world of convenience, security, and control</Text>
+            <Text color={"black"} fontSize={"lg"} textAlign={"center"}>
+              Experience the future of smart living...
+            </Text>
             <Button leftIcon={<MdFileDownload />} colorScheme="blue" variant="solid">
               Coming soon!
             </Button>
+            <Text color={"black"} fontSize={"lg"} textAlign={"center"}>
+              Want to be the first to know? Subscribe now!
+            </Text>
+            {isSubscribed ? (
+              <Text>Thank you for subscribing!</Text>
+            ) : (
+              <Stack spacing={4} direction={"row"}>
+                <Input
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  placeholder="Enter your email"
+                  variant="filled"
+                  type="email"
+                />
+                <Button onClick={handleSubscription}>Subscribe</Button>
+              </Stack>
+            )}
           </Stack>
         </Stack>
       </SimpleGrid>
